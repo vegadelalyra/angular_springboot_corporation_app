b61f897c19f5de106973fa9917ced276baa20e4b
Vegadelalyra <lyradeperseo@gmail.com>
2024-08-01 06:50:08 -0500

    WIP: angular + spring boot corporation app

---
+++ angular.spring.cafe.system/src/test/java/com/vegadelalyra/angular/spring/cafe/system/CafeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vegadelalyra.angular.spring.cafe.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
